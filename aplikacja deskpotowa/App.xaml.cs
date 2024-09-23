fdc0bf48bb76e786b6a81df3af260ec2605c5aab
Emil Skrzydeł <mrolere@Gmail.com>
2024-09-23 19:59:51 +0200

    Add project files.

---
+++ aplikacja deskpotowa/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace aplikacja_deskpotowa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ aplikacja deskpotowa/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace aplikacja_deskpotowa
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private async void zatwierdz(object sender, EventArgs e)
+        {
+            await DisplayAlert("", "Dane pracownika: "+imie.Text+" "+nazwisko.Text+" "+picker.SelectedItem, "OK");
+        }
+
+        private void generuj(object sender, EventArgs e)
+        {
+            if (check1.IsChecked)
+            {
+            }
+        }
+    }
+
+}
